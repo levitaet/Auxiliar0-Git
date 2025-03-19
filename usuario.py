1a17077daaa747905b82240de75f29f2b8af3eda
levitaet <sofia.araya.v@ug.uchile.cl>
2025-03-19 18:44:01 -0300

    borré una línea jiji

--- usuario.py
+++ usuario.py
@@ -2,10 +2,8 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
-
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+        
     def __init__(self, username, password, email):
         self.username = username
         self.password = password
