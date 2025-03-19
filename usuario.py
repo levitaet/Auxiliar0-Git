600cc8aa7852febb4575a6917fb983eb8d25493a
FabianOrmeno <fabian.ormeno@ug.uchile.cl>
2025-03-19 18:40:15 -0300

    holi

--- usuario.py
+++ usuario.py
@@ -1,4 +1,11 @@
 class Usuario:
+    def listarTareas(self):
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"[X] {tarea.obtenerNombre()}" )
+            else:
+                print(f"[ ] {tarea.obtenerNombre()}" )
+
     def __init__(self, username, password, email):
         self.username = username
         self.password = password
